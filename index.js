8e52c480cdf1dd8902b4a730f1eacd724bdcade8
Robert Machado <robert.20.ven@gmail.com>
2023-03-08 16:39:09 -0500

    Estilos y scroll horizontal agredados

--- index.js
+++ index.js
@@ -1,9 +1,56 @@
-  // Obtener el enlace y la sección correspondiente
-  var link = document.querySelector('a[data-filter="pizzas"]');
-  var section = document.querySelector('.menu-item-pizzas');
+  // // Obtener el enlace y la sección correspondiente
+  // const link = document.querySelector('a[data-filter="pizzas"]');
+  // const section = document.querySelector('.menu-item-pizzas');
 
-  // Agregar un evento de clic al enlace
-  link.addEventListener('click', function(e) {
-    e.preventDefault(); // Prevenir la acción predeterminada del enlace
-    section.style.display = 'block'; // Mostrar la sección correspondiente
-  });
+  // // Agregar un evento de clic al enlace
+  // link.addEventListener('click', function(e) {
+  //   e.preventDefault(); // Prevenir la acción predeterminada del enlace
+  //   section.style.display = 'block'; // Mostrar la sección correspondiente
+  // });
+
+  const Scroll = () => {
+    const slider = document.querySelector('.slider');
+    const slides = document.querySelectorAll('.slide');
+    const prevBtn = document.querySelector('.prev');
+    const nextBtn = document.querySelector('.next');
+    const slideWidth = slides[0].clientWidth;
+
+
+    console.log(slider)
+  let slideIndex = 0;
+
+  // Mover el slider a la posición actual
+  const moveSlider = () => {
+    slider.style.transform = `translateX(${-slideIndex * slideWidth}px)`;
+  }
+
+  // Cambiar a la diapositiva anterior
+  const prevSlide = () => {
+    if (slideIndex === 0) {
+      slideIndex = slides.length - 1;
+    } else {
+      slideIndex--;
+    }
+    moveSlider();
+  }
+
+  // Cambiar a la siguiente diapositiva
+  const nextSlide = () => {
+    if (slideIndex === slides.length - 1) {
+      slideIndex = 0;
+    } else {
+      slideIndex++;
+    }
+    moveSlider();
+  }
+
+  // Agregar eventos a los botones de navegación
+  prevBtn.addEventListener('click', prevSlide);
+  nextBtn.addEventListener('click', nextSlide);
+
+};
+
+Scroll();
+
+
+
